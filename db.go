da0515719f132f780deb5ec6ac0aab94611f7f08
andy <150haomengting@163.com>
2020-07-23 14:04:57 +0800

    fix bugs

--- db.go
+++ db.go
@@ -17,9 +17,7 @@ import (
 var db *sqlx.DB
 
 func init() {
-	fmt.Println(1)
 	c := conf.NewDB().Data
-	fmt.Println(2, c)
 	address := fmt.Sprintf("%s:%s@tcp(%s%s)/%s?charset=utf8",
 		c.Uname,
 		c.Passwd,
@@ -27,28 +25,22 @@ func init() {
 		c.Port,
 		c.Database,
 	)
-	fmt.Println(3, address)
 
 	var err error
 	db, err = sqlx.Connect("mysql", address)
-	fmt.Println(4, db, err)
 	if err != nil {
-		defer db.Close()
 		log.Fatalf("open db err: %v", err)
 	}
+	defer db.Close()
 
 	err = db.Ping()
-	fmt.Println(5, err)
 	if err != nil {
-		defer db.Close()
 		log.Fatalf("ping db err: %v", err)
 	}
 
 	db.SetMaxIdleConns(120)
 	db.SetMaxOpenConns(120)
 	db.SetConnMaxLifetime(240 * time.Minute)
-	fmt.Println(6)
 
 	db.Mapper = reflectx.NewMapperFunc("json", strings.ToLower)
-	fmt.Println(7)
 }
